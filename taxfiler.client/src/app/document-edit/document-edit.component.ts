b858792ad4ecb3250c092bce9bc52fb85f299621
Mathias Fritsch <mathias.fritsch.extern@rwe.com>
2025-03-04 08:07:20 +0100

    editable document

---
+++ taxfiler.client/src/app/document-edit/document-edit.component.ts
@@ -0,0 +1,44 @@
+﻿import {Component, Inject} from "@angular/core";
+import {MatFormField} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from "@angular/material/dialog";
+import {MatButton} from "@angular/material/button";
+import {MatInput} from "@angular/material/input";
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: '../document-edit/document-edit.component.html',
+  imports: [
+    MatFormField,
+    FormsModule,
+    MatDialogClose,
+    MatDialogActions,
+    MatButton,
+    MatInput,
+    MatDialogContent,
+    MatDialogTitle
+  ]
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+}
--- taxfiler.client/src/app/documents/documents.component.ts
+++ taxfiler.client/src/app/documents/documents.component.ts
@@ -1,12 +1,19 @@
-import {Component, OnInit} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import { ColDef } from 'ag-grid-community';
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry,RowValueChangedEvent } from 'ag-grid-community';
 import {NgForOf, NgIf} from "@angular/common";
 import {AgGridAngular} from "ag-grid-angular";
+import {Component, OnInit} from '@angular/core';
+import {
+  MatDialog, MatDialogTitle
+} from '@angular/material/dialog';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {DialogOverviewExampleDialog} from "../document-edit/document-edit.component";
+import {MatAnchor, MatButton} from "@angular/material/button";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
+
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
@@ -15,16 +22,23 @@ ModuleRegistry.registerModules([AllCommunityModule]);
     RouterLink,
     NgIf,
     NgForOf,
-    AgGridAngular
+    AgGridAngular,
+    MatButton,
+    MatDialogTitle,
+    MatAnchor
   ],
   standalone: true
 })
 
 export class DocumentsComponent implements  OnInit{
+  editForm: FormGroup;
+
+  animal: string = '';
+  name: string = '';
 
   colDefs: ColDef[] = [
-    { field: 'name', headerName: 'Name', filter: true },
-    { field: 'total' , headerName: 'Brutto', filter: true },
+    { field: 'name', headerName: 'Name', filter: true},
+    { field: 'total' , headerName: 'Brutto', filter: true},
     { field: 'subTotal', headerName:'Netto', filter: true },
     { field: 'taxAmount', headerName:'Steuerbetrag', filter: true },
     { field: 'skonto' , headerName:'Skonto', filter: true},
@@ -35,13 +49,49 @@ export class DocumentsComponent implements  OnInit{
 
   defaultColDef = {
     flex: 1,
+    editable: true,
   };
+
+  onRowValueChanged(event: RowValueChangedEvent) {
+    const data = event.data;
+    console.log(data);
+
+  }
+
 
   public documents: any[] = [];
   public yearMonth: any;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {}
+  constructor(private dialog: MatDialog,
+              private fb: FormBuilder,
+              private http: HttpClient,
+              private route: ActivatedRoute,
+              private router: Router) {
 
+    this.editForm = this.fb.group({
+      documentName: ['']
+    });
+
+  }
+  openEditDocument(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+  onSave(): void {
+    const documentId = this.dialog.getDialogById('id')?.componentInstance.data.id;
+    this.http.post(`/api/documents/updatedocument`, this.editForm.value).subscribe(() => {
+      this.dialog.closeAll();
+      this.getDocuments();
+    });
+  }
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.yearMonth = params.get('yearMonth');
