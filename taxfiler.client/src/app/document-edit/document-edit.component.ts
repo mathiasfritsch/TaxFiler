557c616f8f80b13194f9cdcd6a0e563fb40c62b4
Mathias Fritsch <mathias.fritsch.extern@rwe.com>
2025-03-07 08:16:52 +0100

    localice decimal

--- taxfiler.client/src/app/document-edit/document-edit.component.ts
+++ taxfiler.client/src/app/document-edit/document-edit.component.ts
@@ -1,6 +1,6 @@
-﻿import {Component, Inject} from "@angular/core";
-import {MatFormField} from "@angular/material/form-field";
-import {FormsModule} from "@angular/forms";
+﻿import {Component, Inject, OnInit} from "@angular/core";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -11,34 +11,64 @@ import {
 } from "@angular/material/dialog";
 import {MatButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
-
-export interface DialogData {
-  animal: string;
-  name: string;
-}
+import {Document} from "../model/document";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'dialog-overview-example-dialog',
-  templateUrl: '../document-edit/document-edit.component.html',
+  templateUrl: './document-edit.component.html',
+  styleUrls: ['./document-edit.component.css'],
+  standalone: true,
   imports: [
     MatFormField,
-    FormsModule,
     MatDialogClose,
     MatDialogActions,
     MatButton,
-    MatInput,
     MatDialogContent,
-    MatDialogTitle
+    MatDialogTitle,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInput,
+    MatLabel,
   ]
 })
-export class DialogOverviewExampleDialog {
+
+export class DialogOverviewExampleDialog implements OnInit{
+
+  documentFormGroup: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public document: Document,
+    private fb: FormBuilder
+  ) {
+    this.documentFormGroup = this.fb.group({
+      nameControl: new FormControl(document.name),
+      totalControl: new FormControl(document.total),
+      subTotalControl: new FormControl(document.subTotal),
+    });
+    console.log(document)
+    // this.editDocumentForm = this.fb.group({
+    //   id: [document.id],
+    //   total: [document.total],
+    //   subTotal: [document.subTotal],
+    //   taxAmount: [document.taxAmount],
+    //   skonto: [document.skonto],
+    //   invoiceDate: [document.invoiceDate],
+    //   invoiceNumber: [document.invoiceNumber],
+    //   parsed: [document.parsed]
+    // });
+  }
 
   onCancelClick(): void {
     this.dialogRef.close();
   }
+  onSaveClick(): void {
+
+  }
+  ngOnInit(): void {
+    this.documentFormGroup.value.nameControl = 'asfd'
+  }
 
 }
+
--- taxfiler.client/src/app/documents/documents.component.ts
+++ taxfiler.client/src/app/documents/documents.component.ts
@@ -2,17 +2,25 @@ import { HttpClient } from "@angular/common/http";
 import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import { ColDef } from 'ag-grid-community';
 import { AllCommunityModule, ModuleRegistry,RowValueChangedEvent } from 'ag-grid-community';
-import {NgForOf, NgIf} from "@angular/common";
+import { AG_GRID_LOCALE_DE } from '@ag-grid-community/locale';
+import { NgIf} from "@angular/common";
 import {AgGridAngular} from "ag-grid-angular";
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, LOCALE_ID, Inject} from '@angular/core';
 import {
   MatDialog, MatDialogTitle
 } from '@angular/material/dialog';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
 import {DialogOverviewExampleDialog} from "../document-edit/document-edit.component";
 import {MatAnchor, MatButton} from "@angular/material/button";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
+
+function formatPrice(value: any):string{
+  return value.value ? value.value.toLocaleString('de-DE', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }) : '';
+}
 
 @Component({
   selector: 'app-documents',
@@ -21,35 +29,63 @@ ModuleRegistry.registerModules([AllCommunityModule]);
   imports: [
     RouterLink,
     NgIf,
-    NgForOf,
     AgGridAngular,
     MatButton,
     MatDialogTitle,
-    MatAnchor
+    MatAnchor,
   ],
   standalone: true
 })
 
 export class DocumentsComponent implements  OnInit{
-  editForm: FormGroup;
 
-  animal: string = '';
-  name: string = '';
+
 
   colDefs: ColDef[] = [
-    { field: 'name', headerName: 'Name', filter: true},
-    { field: 'total' , headerName: 'Brutto', filter: true},
-    { field: 'subTotal', headerName:'Netto', filter: true },
-    { field: 'taxAmount', headerName:'Steuerbetrag', filter: true },
-    { field: 'skonto' , headerName:'Skonto', filter: true},
+    {
+      field: 'name',
+      headerName: 'Name',
+      filter: true,
+      cellStyle: {
+        textAlign: 'left'
+      }
+    },
+    {
+      field: 'total' ,
+      headerName: 'Brutto',
+      valueFormatter: formatPrice,
+    },
+    {
+      field: 'subTotal',
+      headerName:'Netto',
+      valueFormatter: formatPrice,
+    },
+    {
+      field: 'taxAmount',
+      headerName:'Steuerbetrag',
+      valueFormatter: formatPrice,
+    },
+    {
+      field: 'skonto' ,
+      headerName:'Skonto',
+      valueFormatter: formatPrice,
+    },
     { field: 'invoiceDate', headerName:'Rechungsdatum', filter: true },
-    { field: 'invoiceNumber' , headerName:'Rechnungsnummer', filter: true},
+    {
+      field: 'invoiceNumber' ,
+      headerName:'Rechnungsnummer',
+      cellStyle: { textAlign: 'left' }
+    },
     { field: 'parsed', headerName:'Parsed', filter: true},
   ];
 
   defaultColDef = {
     flex: 1,
-    editable: true,
+    filter: true,
+    cellStyle: {
+      textAlign: 'right',
+      paddingRight: '30px'
+    }
   };
 
   onRowValueChanged(event: RowValueChangedEvent) {
@@ -57,46 +93,36 @@ export class DocumentsComponent implements  OnInit{
     console.log(data);
 
   }
-
-
-  public documents: any[] = [];
+  public today:Date = new Date();
+  public documents: Document[] = [];
   public yearMonth: any;
+  public amount: number = 10.24;
+  localeText = AG_GRID_LOCALE_DE;
 
   constructor(private dialog: MatDialog,
               private fb: FormBuilder,
               private http: HttpClient,
               private route: ActivatedRoute,
-              private router: Router) {
-
-    this.editForm = this.fb.group({
-      documentName: ['']
-    });
-
+              @Inject(LOCALE_ID) public locale: string) {
   }
+
   openEditDocument(): void {
     const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      width: '250px',
-      data: {name: this.name, animal: this.animal}
+      width: '600px',
+      data: this.documents[0]
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      this.animal = result;
     });
   }
 
-  onSave(): void {
-    const documentId = this.dialog.getDialogById('id')?.componentInstance.data.id;
-    this.http.post(`/api/documents/updatedocument`, this.editForm.value).subscribe(() => {
-      this.dialog.closeAll();
-      this.getDocuments();
-    });
-  }
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.yearMonth = params.get('yearMonth');
       this.getDocuments();
     });
+
   }
   getDocuments() {
     this.http.get<any[]>(`/api/documents/getdocuments`).subscribe(
