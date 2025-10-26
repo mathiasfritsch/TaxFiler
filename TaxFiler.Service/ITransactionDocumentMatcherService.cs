3c650f873ed4522a196f9def145c4790ccaa0a3f
mathiasfritsch <mathias.fritsch.hh@gmail.com>
2025-10-26 20:54:36 +0100

    Remove `TransactionDocumentMatcherService` and its interface.
    This commit deletes the `TransactionDocumentMatcherService` class and the corresponding `ITransactionDocumentMatcherService` interface. The functionality is no longer required, and the unused code has been removed to improve maintainability.

--- TaxFiler.Service/ITransactionDocumentMatcherService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TaxFiler.Model.Dto;
-using TransactionDto = TaxFiler.Model.Csv.TransactionDto;
-
-namespace TaxFiler.Service;
-
-public interface ITransactionDocumentMatcherService
-{
-    public DocumentDto[] MatchTransactionToDocument(TransactionDto transaction);
-}
