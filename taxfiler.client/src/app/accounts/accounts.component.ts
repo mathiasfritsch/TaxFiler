a70cc9251e5f9e82534d5cc074af6e8a03b6a9e6
Fritsch, Mathias <mathias.fritsch.extern@rwe.com>
2026-01-16 19:48:50 +0100

    Refactor accounts component to use observable for data binding

--- taxfiler.client/src/app/accounts/accounts.component.ts
+++ taxfiler.client/src/app/accounts/accounts.component.ts
@@ -1,55 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Account } from '../model/account';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { AgGridModule } from 'ag-grid-angular';
-import { ColDef, GridReadyEvent } from 'ag-grid-community';
-import { ButtonCellRendererComponent } from '../button-cell-renderer/button-cell-renderer.component';
-import { RouterModule, ActivatedRoute, Router } from '@angular/router';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef, AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { AG_GRID_LOCALE_DE } from '@ag-grid-community/locale';
 import { NavigationComponent } from '../shared/navigation/navigation.component';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
   imports: [
+    AgGridAngular,
+    CommonModule,
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
-    AgGridModule,
-    RouterModule,
     NavigationComponent
-],
+  ],
   templateUrl: './accounts.component.html',
   styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent implements OnInit {
-  accounts: Account[] = [];
+  accounts$: Observable<Account[]> | undefined;
   colDefs: ColDef[] = [];
   yearMonth: string = '';
   defaultColDef: ColDef = {
+    flex: 1,
     sortable: true,
     filter: true,
     resizable: true
   };
-  localeText = {
-    page: 'Seite',
-    more: 'Mehr',
-    to: 'bis',
-    of: 'von',
-    next: 'Nächste',
-    previous: 'Vorherige',
-    loadingOoo: 'Lädt...',
-    noRowsToShow: 'Keine Konten gefunden'
-  };
+  localeText = AG_GRID_LOCALE_DE;
 
   constructor(
     private http: HttpClient,
     private dialog: MatDialog,
-    private route: ActivatedRoute,
-    private router: Router
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
@@ -69,13 +63,6 @@ export class AccountsComponent implements OnInit {
   }
 
   getAccounts(): void {
-    this.http.get<Account[]>('/api/accounts/getaccounts').subscribe({
-      next: (accounts) => {
-        this.accounts = accounts;
-      },
-      error: (error) => {
-        console.error('Error fetching accounts:', error);
-      }
-    });
+    this.accounts$ = this.http.get<Account[]>('/api/accounts/getaccounts');
   }
 }
